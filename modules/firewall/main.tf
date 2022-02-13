9240c2aa60f1a6bcffd9f7245b631364c5411e92
Michael Bychkowski <mbychkowski@google.com>
2022-02-13 03:24:12 +0000

    Rename subnetwork block

--- modules/firewall/main.tf
+++ modules/firewall/main.tf
@@ -48,7 +48,7 @@ resource "google_compute_router_nat" "nat" {
     nat_ip_allocate_option             = "AUTO_ONLY"
     source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
     
-    subnetworks = [
+    subnetwork = [
         {
             name                    = "${var.env}-subnet-vm"
             source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
