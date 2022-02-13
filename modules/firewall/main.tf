ad7ee5fa4e5fdfd38daa495c4f87ee5e91271a41
Michael Bychkowski <mbychkowski@google.com>
2022-02-13 03:31:37 +0000

    Try to create two subnetwork in nat block

--- modules/firewall/main.tf
+++ modules/firewall/main.tf
@@ -48,16 +48,15 @@ resource "google_compute_router_nat" "nat" {
     nat_ip_allocate_option             = "AUTO_ONLY"
     source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
     
-    subnetwork = [
-        {
-            name                    = "${var.env}-subnet-vm"
-            source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
-        },
-        {
-            name                    = "${var.env}-subnet-gke"
-            source_ip_ranges_to_nat = ["ALL_IP_RANGES"]            
-        }
-    ] 
+    subnetwork {
+        name                    = "${var.env}-subnet-vm"
+        source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+    }
+
+    subnetwork  {
+        name                    = "${var.env}-subnet-gke"
+        source_ip_ranges_to_nat = ["ALL_IP_RANGES"]            
+    }
 
     log_config {
         enable = true
