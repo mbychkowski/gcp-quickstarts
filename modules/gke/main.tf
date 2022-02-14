0b9531834ba36dc7032228c909406a7c76c9cb7f
Michael Bychkowski <mbychkowski@google.com>
2022-02-14 14:30:20 +0000

    Try to boostrap GKE cluster without acm

--- modules/gke/main.tf
+++ modules/gke/main.tf
@@ -16,66 +16,72 @@
 
 
  module "gke" {
-    source             = "terraform-google-modules/kubernetes-engine/google"
-    version            = "~> 16.0"
-    project_id         = var.project
-    name               = "sfl-acm"
-    region             = var.region
-    zones              = [var.zone]
-    initial_node_count = 4
-    network            = var.network
-    subnetwork         = var.subnet
-    ip_range_pods      = "${var.env}-pod"
-    ip_range_services  = "${var.env}-svc"
+    source                     = "terraform-google-modules/kubernetes-engine/google"
+    version                    = "~> 16.0"
+    project_id                 = var.project
+    name                       = "sfl-acm"
+    region                     = var.region
+    zones                      = [var.zone]
+    initial_node_count         = 1
+    network                    = var.network
+    subnetwork                 = var.subnet
+    ip_range_pods              = "${var.env}-pod"
+    ip_range_services          = "${var.env}-svc"
+    horizontal_pod_autoscaling = true
+    remove_default_node_pool   = true
 
     node_pools = [
         {
-            name         = "pool-01"
-            machine_type = "e2-standard-2"
-            auto_repair  = true
-            auto_upgrade = true
-            enable_secure_boot = true
+            name                        = "pool-01"
+            machine_type                = "e2-standard-2"
+            auto_repair                 = true
+            auto_upgrade                = true
+            enable_secure_boot          = true
             enable_integrity_monitoring = true
-            enable_shielded_nodes = true
+            enable_shielded_nodes       = true
+            node_count                  = 4
         }
     ]
 
 }
 
-resource "google_gke_hub_membership" "membership" {
-  provider      = google-beta
-  membership_id = "membership-hub-${module.gke.name}"
-  endpoint {
-    gke_cluster {
-      resource_link = "//container.googleapis.com/${module.gke.cluster_id}"
-    }
-  }
-}
+# resource "google_gke_hub_membership" "membership" {
+#     provider      = google-beta
+#     membership_id = "membership-hub-${module.gke.name}"
+#     endpoint {
+#       gke_cluster {
+#         resource_link = "https://container.googleapis.com/v1/${google_container_cluster.primary.id}"
+#       }
+#     }
+#     authority {
+#       issuer = "https://container.googleapis.com/v1/${google_container_cluster.primary.id}"
+#     }
+# }
 
-resource "google_gke_hub_feature" "configmanagement_acm_feature" {
-  name     = "configmanagement"
-  location = "global"
-  provider = google-beta
-}
+# resource "google_gke_hub_feature" "configmanagement_acm_feature" {
+#     name     = "configmanagement"
+#     location = "global"
+#     provider = google-beta
+# }
 
-resource "google_gke_hub_feature_membership" "feature_member" {
-  provider   = google-beta
-  location   = "global"
-  feature    = "configmanagement"
-  membership = google_gke_hub_membership.membership.membership_id
-  configmanagement {
-    version = "1.8.0"
-    config_sync {
-      source_format = "unstructured"
-      git {
-        sync_repo   = var.sync_repo
-        sync_branch = var.sync_branch
-        policy_dir  = var.policy_dir
-        secret_type = "none"
-      }
-    }
-  }
-  depends_on = [
-    google_gke_hub_feature.configmanagement_acm_feature
-  ]
-}
+# resource "google_gke_hub_feature_membership" "feature_member" {
+#     provider   = google-beta
+#     location   = "global"
+#     feature    = "configmanagement"
+#     membership = google_gke_hub_membership.membership.membership_id
+#     configmanagement {
+#       version = "1.8.0"
+#       config_sync {
+#         source_format = "unstructured"
+#         git {
+#             sync_repo   = var.sync_repo
+#             sync_branch = var.sync_branch
+#             policy_dir  = var.policy_dir
+#             secret_type = "none"
+#         }
+#       }
+#     }
+#     depends_on = [
+#         google_gke_hub_feature.configmanagement_acm_feature
+#     ]
+# }
