00eda04824f6d8b5b7f32623d6ffa5c6db9c4de0
Michael Bychkowski <mbychkowski@google.com>
2022-04-25 13:38:41 -0400

    Remove installation of ASM

--- terraform-asm/environments/dev/main.tf
+++ terraform-asm/environments/dev/main.tf
@@ -4,14 +4,8 @@ locals {
 
 data "google_client_config" "default" {}
 
-provider "google-beta" {
-  version = "~> 3.79.0"
-  region  = var.region
-}
-
-provider "google" {
-  version = "~> 3.63.0"
-  region  = var.region
+data "google_project" "project" {
+  project_id = var.project_id
 }
 
 provider "kubernetes" {
@@ -20,13 +14,9 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(module.gke.ca_certificate)
 }
 
-data "google_project" "project" {
-  project_id = var.project_id
-}
-
 module "gke" {
   source                  = "terraform-google-modules/kubernetes-engine/google"
-  version                 = "18.0.0"
+  version                 = "20.0.0"
 
   project_id              = var.project_id
   name                    = "${local.cluster_type}-cluster-${var.cluster_name_suffix}"
@@ -52,33 +42,33 @@ module "gke" {
   ]
 }
 
-module "asm" {
-  source                = "terraform-google-modules/kubernetes-engine/google//modules/asm"
-  version               = "18.0.0"
-  asm_version	        = var.asm_version
-  cluster_name          = module.gke.name
-  project_id            = var.project_id
-  location              = module.gke.location
-  cluster_endpoint      = module.gke.endpoint
-  enable_all            = false
-  enable_cluster_roles  = true
-  enable_cluster_labels = false
-  enable_gcp_apis       = true
-  enable_gcp_iam_roles  = true
-  enable_gcp_components = true
-  enable_registration   = false
-  managed_control_plane = false
-  service_account       = "${var.terraform_sa}@${var.project_id}.iam.gserviceaccount.com"
-  key_file              = "./${var.terraform_sa}.json"
-  options               = ["envoy-access-log,egressgateways"]
-  custom_overlays       = ["./custom_ingress_gateway.yaml"]
-  skip_validation       = false
-  outdir                = "./${module.gke.name}-outdir-${var.asm_version}"
-  # ca                    = "citadel"
-  # ca_certs = {
-  #   "ca_cert"    = "./ca-cert.pem"
-  #   "ca_key"     = "./ca-key.pem"
-  #   "root_cert"  = "./root-cert.pem"
-  #   "cert_chain" = "./cert-chain.pem"
-  # }
-}
+# module "asm" {
+#   source                = "terraform-google-modules/kubernetes-engine/google//modules/asm"
+#   version               = "18.0.0"
+#   asm_version	        = var.asm_version
+#   cluster_name          = module.gke.name
+#   project_id            = var.project_id
+#   location              = module.gke.location
+#   cluster_endpoint      = module.gke.endpoint
+#   enable_all            = false
+#   enable_cluster_roles  = true
+#   enable_cluster_labels = false
+#   enable_gcp_apis       = true
+#   enable_gcp_iam_roles  = true
+#   enable_gcp_components = true
+#   enable_registration   = false
+#   managed_control_plane = false
+#   service_account       = "${var.terraform_sa}@${var.project_id}.iam.gserviceaccount.com"
+#   key_file              = "./${var.terraform_sa}.json"
+#   options               = ["envoy-access-log,egressgateways"]
+#   custom_overlays       = ["./custom_ingress_gateway.yaml"]
+#   skip_validation       = false
+#   outdir                = "./${module.gke.name}-outdir-${var.asm_version}"
+#   # ca                    = "citadel"
+#   # ca_certs = {
+#   #   "ca_cert"    = "./ca-cert.pem"
+#   #   "ca_key"     = "./ca-key.pem"
+#   #   "root_cert"  = "./root-cert.pem"
+#   #   "cert_chain" = "./cert-chain.pem"
+#   # }
+# }
